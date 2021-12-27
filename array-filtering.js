a985ec2948045f13f6ec541f4589577ed0f51b46
JackyLin1 <jlin9702@gmail.com>
2021-12-27 01:26:47 +0000

    array filter

---
+++ array-filtering.js
@@ -0,0 +1,8 @@
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let filtered = number.filter(evenNumbers)
+
+function evenNumbers (number) {  
+  return number % 2 === 0  
+} 
+
+console.log(filtered)
